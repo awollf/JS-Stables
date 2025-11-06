8adf1e6f1b143de2a7f5c4d91edd482368afedf3
Alyssa <awollf1@my.bcit.ca>
2025-11-06 13:15:42 -0800

    add lab 2

--- stables.js
+++ stables.js
@@ -1,73 +1,81 @@
-let horseOneName= "Pebbles";
-let horseOneAge= 15;
-let isHorseOneInStable= true;
-let monthlyFee= 200;
-let latePayment= monthlyFee * 1.2;
-let welcomeMessage= "Welcome to my stables!"
-let latePaymentMessage = "Unfortunately, your monthly bording fee is late. You now owe " + (monthlyFee + lateFee) + ". Please pay us immediately!"
-let introduceHorseOne= "This is Pebbles! He is " + horseOneAge + "years old!"
+let horseOneName = "Pebbles";
+let horseOneAge = 15;
+let isHorseOneInStable = true;
+let monthlyFee = 200;
+let latePayment = monthlyFee * 1.2;
+
+let welcomeMessage = "Welcome to my stables!";
+let latePaymentMessage = "Unfortunately, your monthly boarding fee is late. You now owe $" + latePayment + ". Please pay us immediately!";
+let introduceHorseOne = "This is " + horseOneName + "! He is " + horseOneAge + " years old!";
 
 let horses = {
-    horseOne: {
-        name: "Pebbles",
-        age:15, 
-        nickname: "Pebbs",
-        favoriteTreat: "Carrots", 
-        monthlyRent: 200,
-        isInStable: true, 
-        color: "Blue",
-        canFly: true, 
-        isHungry: true
-    }
-    horseTwo: {
-        name: "Toffee",
-        age: 12,
-        nickname: "Tofu",
-        favoriteTreat: "Crackers",
-        monthlyRent: 300,
-        isInStable: false,
-        color: "Red",
-        canFly: false,
-        isHungry: true }
-        
-    horseThree: {
-        name: "Sprite",
-        age:6,
-        nickname:"Spit",
-        favoriteTreat:"Champagne", 
-        monthlyRent: 200,
-        isInStable:false,
-        color: "Light Green",
-        canFly:true,
-        isHungry: false}
-    }
+  horseOne: {
+    name: "Pebbles",
+    age: 15,
+    nickname: "Pebbs",
+    favoriteTreat: "Carrots",
+    monthlyRent: 200,
+    isInStable: true,
+    color: "Blue",
+    canFly: true,
+    isHungry: true,
+  },
+  horseTwo: {
+    name: "Toffee",
+    age: 12,
+    nickname: "Tofu",
+    favoriteTreat: "Crackers",
+    monthlyRent: 300,
+    isInStable: false,
+    color: "Red",
+    canFly: false,
+    isHungry: true,
+  },
+  horseThree: {
+    name: "Sprite",
+    age: 6,
+    nickname: "Spit",
+    favoriteTreat: "Champagne",
+    monthlyRent: 200,
+    isInStable: false,
+    color: "Light Green",
+    canFly: true,
+    isHungry: false,
+  },
+};
 
-    console.log(horseOne.isHungry);
-    console.log(horseTwo.isHungry);
-    console.log(horseThree.isHungry);
+// Access individual horses for easier logging
+let { horseOne, horseTwo, horseThree } = horses;
 
-let horses = [horseOne, horseTwo, horseThree, horseFour, horseFive];
-    console.log(horses);
+console.log(horseOne.isHungry);
+console.log(horseTwo.isHungry);
+console.log(horseThree.isHungry);
 
-    // LAB 2
+// Create an array of horses
+let horseList = [horseOne, horseTwo, horseThree];
+console.log(horseList);
 
+// LAB 2
 let usedStalls = 3;
-let totalStalls= 6;
+let totalStalls = 6;
 
-function checkAvailableStalls(total, used){
-    let available = totalStalls - usedStalls; 
-console.log("there are " + available + " stalls Available.")
+function checkAvailableStalls(total, used) {
+  let available = total - used;
+  console.log("There are " + available + " stalls available.");
+  return available;
 }
-checkAvailableStalls(totalStalls, usedStalls);
 
-console.log(availableStalls);
+let availableStalls = checkAvailableStalls(totalStalls, usedStalls);
+console.log("Available stalls: ", availableStalls);
 
-function latePayment(horse) {
-    return horse.monthlyRent * 1.2
+function calculateLatePayment(horse) {
+  return horse.monthlyRent * 1.2;
 }
-console.log(latePayment(horseThree));
 
-function getNickname(horse){return horse.nickname;}
-console.log(getNickname(horseTwo));
+console.log("Late payment for Sprite: $" + calculateLatePayment(horseThree));
 
+function getNickname(horse) {
+  return horse.nickname;
+}
 
+console.log("Toffee's nickname: " + getNickname(horseTwo));
